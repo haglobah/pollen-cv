261ffeb378bd963b89bd37ff967db047d1eeffe2
haglobah <bah@posteo.de>
2022-11-13 22:38:50 +0100

    Create the english version

--- Résumé.poly.pm
+++ Résumé.poly.pm
@@ -7,48 +7,76 @@
 
 ◊; }
 
-◊section["Fähigkeiten & Auszeichnungen"]{
+◊section["Skills & Abilities"]{
 
 	◊par{
-		Programmierkenntnisse in  Haskell, Python, Clojure, Common Lisp, C#, C++, C, Racket und Rust • Zertifikat Vertiefungskurs Mathematik • Teilnahme an Jugend forscht 
+		◊role{Programming Languages}
+		◊themes{Haskell, Python, Clojure, Common Lisp, C#, C++, C, Racket, Rust}
+
+		◊role{Organizational Development}
+		◊themes{Sociocracy, The Lean Startup, Start with Why, Reinventing Organizations}
+
+		◊role{Design}
+		◊themes{Design Thinking, Systems Design, Designing your Life}
 	}
 }
 
-◊section["Arbeitserfahrung"]{
+◊section["Work Experience"]{
 
-	◊par[#:org "Hacker School gGmbH" #:area "NGO: Bildung" #:role "Werkstudent" #:date "02/2021-Heute" #:loc "Hamburg/Remote" #:topics "Organisationsentwicklung, Programmieren"]{
+	◊par[#:org "Hacker School gGmbH" #:area "NGO: Education" #:role "Working Student" #:date "02/2021-today" #:loc "Hamburg/Remote" #:topics "Sociocracy, Racket, Teaching"]{
 
-		(Weiter-)Entwicklung von Kurskonzepten, Durchführung von Kursen als Team-Inspirer sowie die (technologische) Weiterentwicklung der Hacker School selbst.
+		◊ul{
+			◊li{Developed & held courses}
+			◊li{Created a course website with ◊l["pollenpub.com"]{Pollen}}
+			◊li{Introduced the usage of a knowledge base system}
+		}
 
 	}
-	◊par[#:org "Deutsche SchülerAkademie" #:area "" #:role "Kursleitung" #:date "16 Tage | 06-07/2022" #:loc "Wolfsberg" #:topics ""]{
+	◊par[#:org "Deutsche SchülerAkademie" #:area "" #:role "Course Instructor" #:date "16 days | 06-07/2022" #:loc "Wolfsberg" #:topics "Learning, Teaching" #:show-tops? #t]{
 
-		Konzeption, Vorbereitung und Leitung des ca. 50-stündigen Kurses ◊e{Wie Menschen lernen} für leistungsbereite und -fähige Schüler*innen der 10.-11. Klasse, sowie deren Betreuung über den Verlauf der Akademie.
+		Created a concept for, prepared and taught a 50-hour university level course named ◊e{How People Learn} for exceptionally strong students (age 16-18). Co-supervised all the students over the course of the academy.
 	}
-	◊par[#:org "rootlinks" #:area "NGO: Bildung, Nachhaltigkeit" #:role "ehrenamtlich" #:date "06/2021-03/2022" #:loc "Zürich" #:topics "Organisationsentwicklung, Nachhaltigkeit, Design Thinking, Systems Design, Coaching"]{
+	◊par[#:org "rootlinks" #:area "NGO: Education, Sustainability" #:role "voluntary" #:date "06/2021-03/2022" #:loc "Zürich" #:topics "Sociocracy, Sustainability, Design Thinking, Systems Design, Teaching, Coaching, Recruiting"]{
 		
-		Mitglied der Gesamtorganisation sowie des Learning Journey-Kreises.
-		Planung und Durchführung von wöchentlichen Inputs/Workshops. Coaching eines Challenge-Teams sowie Mitarbeit an der Organisationsstruktur und  der Knowledge Base.
+		◊; General Circle, Learning Journey Circle, Recruiting Circle
+		◊ul{
+			◊li{Planned and organized the Learning Journey of two semesters}
+			◊li{Held inputs and workshops}
+			◊li{Coached a challenge team}
+			◊li{Contributed to creating – and evolving – a working organizational structure, as well as a knowledge base}
+			◊li{Planned and executed the Recruiting of the 3rd batch.}
+		}
 	}
-	◊par[#:org "active group GmbH" #:area "Softwareentwicklung" #:role "Praktikum" #:date "03/2020-05/2020" #:loc "Tübingen" #:topics "Machine Learning, Tensorflow, Emacs, magit"]{
+	◊par[#:org "Spatzennest Tübingen" #:area "" #:role "Team Leader – once" #:date "2017-today" #:loc "Tübingen" #:topics ""]{
+		
+		Led a team (of group leaders and children) over the course of the 3-week kid's camp.
+		Planned and conducted the preparatory weekends and moderated plenary sessions.
+		◊; Feste?
+	}
+	◊par[#:org "" #:area "" #:role "Group Leader – eight times" #:date "" #:loc "Tübingen" #:topics ""]{
+		
+		Paedagogically supervised 12-20 children.
+	}
+	◊par[#:org "active group GmbH" #:area "Software Development" #:role "Intern" #:date "03/2020-05/2020" #:loc "Tübingen" #:topics "Haskell, Clojure, Machine Learning, Emacs, magit"]{
 
-		Entwicklung von Software mithilfe funktionaler Programmierung in Haskell (Machine Learning) und Clojure (Frontend). 
+		Implemented a Web Server for a Machine Learning Project in Haskell.
+		Developed parts of the project's frontend in ClojureScript (both solo and pair programming).
 
 	}
-	◊par[#:org "TNG Technology Consulting" #:area "Tech-Consulting" #:role "Praktikum" #:date "12/2019-03/2020" #:loc "München" #:topics "C#, Python, Unity, Reinforcement Learning, Tensorflow, git"]{
+	◊par[#:org "TNG Technology Consulting" #:area "Technology Consulting" #:role "Intern" #:date "12/2019-03/2020" #:loc "Munich" #:topics "C#, Python, Unity, Reinforcement Learning, Tensorflow, git"]{
 
-		Praktikum bei TNG im Innovation Hacking. Entwicklung von KI-Agenten im Spiel Blobby VR; Debugging des Spiels.
-
+		Developed KI-Agents for the Game Blobby VR.
+		Looked after ◊;{managed?} the TNG booth at OOP 2020.
 	}
-	◊par[#:org "bild der wissenschaft/natur" #:area "Redaktion" #:role "Praktikum" #:date "5 Tage | 09/2018" #:loc "Leinfelden-Echterdingen" #:topics ""]{
-
-
-	}
-	◊par[#:org "Boehringer Ingelheim" #:area "Pharmaindustrie" #:role "Praktikum" #:date "5 Tage | 09/2017" #:loc "Biberach a.d. Riß" #:topics ""]{
+	◊par[#:org "bild der wissenschaft/natur" #:area "Editorial Office" #:role "Intern" #:date "5 days | 09/2018" #:loc "Leinfelden-Echterdingen" #:topics ""]{
 
 
 	}
-	◊par[#:org "SmiTo GmbH" #:area "Ingenieurbüro" #:role "Praktikum" #:date "5 Tage | 10/2015" #:loc "Tübingen" #:topics ""]{
+	◊par[#:org "Boehringer Ingelheim" #:area "Pharmacology" #:role "Intern" #:date "5 days | 09/2017" #:loc "Biberach a.d. Riß" #:topics ""]{
+
+
+	}
+	◊par[#:org "SmiTo GmbH" #:area "Engineering" #:role "Intern" #:date "5 days | 10/2015" #:loc "Tübingen" #:topics ""]{
 
 
 	}
@@ -58,44 +86,42 @@
 	◊; }
 }
 
-◊section["Bildungsweg"]{
+◊section["Education"]{
 
-	◊par[#:org "42 Heilbronn" #:area "Programmierschule" #:role "Student" #:date "03/2022-Heute" #:loc "" #:topics ""]{
+	◊par[#:org "42 Heilbronn" #:area "Programming School" #:role "Student" #:date "03/2022-Heute" #:loc "" #:topics "C, Racket"]{
 
-		◊e{Erstellung und Verwaltung der ◊l["https://haglobah.github.io/Mastering-42/"]{Mastering 42}-Website}
+		◊e{Creation and Maintenance of the ◊l["https://haglobah.github.io/Mastering-42/"]{Mastering 42-Website}}
 	}
-	◊par[#:org "ETH Zürich" #:area "" #:role "BSc Physik" #:date "14/09/2020-20/02/22" #:loc "Zürich" #:topics ""]{
+	◊par[#:org "ETH Zürich" #:area "" #:role "BSc Physics" #:date "09/2020-02/2022" #:loc "Zürich" #:topics ""]{
 
-		◊e{bis zum 3. Fachsemester}
-		◊(br)
-		◊e{03/21 - 07/21 Teilnahme an ◊l["https://www.rootlinks.ch"]{rootlinks}}
+		Up until the 3rd semester.
+		◊(br)◊(br)
+		◊e{03/21 - 07/21 Participation in the ◊l["https://www.rootlinks.ch"]{rootlinks}} programme
 	}
-	◊par[#:org "Leibniz Kolleg" #:area "" #:role "Studium generale et sociale" #:date "03/10/2018-26/07/2019" #:loc "Tübingen" #:topics ""]{
+	◊par[#:org "Leibniz Kolleg" #:area "" #:role "Studium generale et sociale" #:date "10/2018-07/2019" #:loc "Tübingen" #:topics ""]{
 
-		◊e{Pflichtseminare: Wissenschaftliches Schreiben, Rhetorik, Naturwissenschaftliches Schreiben, Seminar zum Erstellen wissenschaftlicher Poster, Politisches Seminar (in Kollaboration mit der lpb Baden-Württemberg), Studienreise nach Rom mit Fokus auf Kunst und Archäologie}
+		Obligatory seminars: Scientific Writing, Rhetorics, Writing for science, Seminar for scientific posters, Politics (in collaboration with lpb Baden-Württemberg), study trip to rome with focus on arts and archeology
 		◊(br)◊(br)
-		◊e{belegte Kurse: Psychologie, Medizin, Virologie, Philosophie, Chor, Astrophysik, Sinologie, Pädagogik, Wissenschaftstheorie, Volkswirtschaftslehre, Mathematik, Informatik, Theater
+		Courses: Psychology, Medicine, Virology, Philosophy, Choir, Astrophysics, Sinology, Education, Theory of Science and Philosophy of Mind, Economics, Mathematics, Computer Science, Theater 
 		◊(br)◊(br)
-		Zwei verfasste Trimesterarbeiten:
+		Two written term papers:
 		◊ul{
-			◊li{Über die Grenzen von Moral - Philosophie}
-			◊li{Ein neues Lernsystem - Psychologie}
-		}}
+			◊li{Über die Grenzen von Moral - Philosophy}
+			◊li{Ein neues Lernsystem - Psychology}
+		}
 	}
 	◊par[#:org "Carlo-Schmid-Gymnasium" #:area "" #:role "" #:date "2010-2018" #:loc "Tübingen" #:topics ""]{
 
-		◊e{Abschlussnote 1,0; Preis für den besten Abiturienten in Chemie}
+		Abschlussnote 1,0; Preis für den besten Abiturienten in Chemie
 		◊(br)◊(br)
-		◊e{zusätzliche Tätigkeiten: ◊(br)
+		◊e{
 		◊table{
-			2016 - 2018,Mitglied der Jazz AG des CSG Tübingen (Bass)
-			2014 - 2018,Leitung einer Jungschar
-			2016       ,Betreuung von Flüchtlingen über ein halbes Jahr
-			2012 - 2015,Teilnahme am SchülerForschungsZentrum Tübingen
-			2011 - 2015,Gitarrenunterricht
+			2016 - 2018,Part of the Jazz club of the CSG Tübingen (Bass)
+			2014 - 2018,Leader of a christian youth group
+			2016       ,Mentoring of refugees for half a year
+			2012 - 2015,Participation at the student research centre
 			}
 		}
-
 	}
 	◊par[#:org "Grundschule Hechinger Eck" #:area "" #:role "" #:date "2006-2010" #:loc "Tübingen" #:topics ""]{
 
@@ -107,16 +133,16 @@
 	◊; }
 }
 
-◊section["ehrenamtliches Engagement"]{
+◊; ◊section["ehrenamtliches Engagement"]{
 
-	◊par[#:org "" #:area "" #:role "" #:date "" #:loc "" #:topics ""]{
+◊; 	◊par[#:org "" #:area "" #:role "" #:date "" #:loc "" #:topics ""]{
 
-		◊e{
-		◊table{
-			seit 03/20   ,Inspirer bei der Hacker School
-			seit 2016    ,Gruppenleiter im Spatzennest Tübingen in acht Freizeiten; Teamleiter in einer Freizeit
-			2018 - 2020  ,Nachhilfelehrer in verschiedenen Fächern
-		}
-		}
-	}
-}
+◊; 		◊e{
+◊; 		◊table{
+◊; 			seit 03/20   ,Inspirer bei der Hacker School
+◊; 			seit 2016    ,Gruppenleiter im Spatzennest Tübingen in acht Freizeiten; Teamleiter in einer Freizeit
+◊; 			2018 - 2020  ,Nachhilfelehrer in verschiedenen Fächern
+◊; 		}
+◊; 		}
+◊; 	}
+◊; }
