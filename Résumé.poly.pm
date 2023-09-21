97f522f45e652706b0fe1df4e9187dd63007da15
Beat Hagenlocher <bah@posteo.de>
2023-09-21 15:18:28 +0200

    Add shorter versions

--- Résumé.poly.pm
+++ Résumé.poly.pm
@@ -37,28 +37,32 @@
 	}
 	◊par[#:org "Deutsche SchülerAkademie" #:area "" #:role "Course Instructor" #:date "16 days | 06-07/2022" #:loc "Wolfsberg" #:topics "Learning, Teaching" #:show-tops? #t]{
 
-		Created a concept for, prepared and taught a 50-hour university level course named ◊e{How People Learn} for exceptionally strong students (age 16-18). Co-supervised all the students over the course of the academy.
+		Taught a 50-hour university-level course called ◊e{How People learn}.
+
+		◊; Taught a 50-hour university level course called ◊e{How People Learn} to exceptionally strong students (age 16-18). Co-supervised all the students over the course of the academy.
 	}
 	◊par[#:org "rootlinks" #:area "NGO: Education, Sustainability" #:role "voluntary" #:date "06/2021-03/2022" #:loc "Zürich" #:topics "Sociocracy, Sustainability, Design Thinking, Systems Design, Teaching, Coaching, Recruiting"]{
 		
+		Organized the rootlinks programme for two semesters.
+
 		◊; General Circle, Learning Journey Circle, Recruiting Circle
-		◊ul{
-			◊li{Planned and organized the Learning Journey of two semesters}
-			◊li{Held inputs and workshops}
+		◊;{◊ul{
+			◊li{Organized the Learning Journey of two semesters, held inputs and workshops}
 			◊li{Coached a challenge team}
 			◊li{Contributed to creating – and evolving – a working organizational structure, as well as a knowledge base}
 			◊li{Planned and executed the Recruiting of the 3rd batch.}
 		}
+		}
 	}
 	◊par[#:org "Spatzennest Tübingen" #:area "" #:role "Team Leader – twice" #:date "2017-today" #:loc "Tübingen" #:topics ""]{
 		
-		Led a team (of group leaders and children) over the course of the 3-week kid's camp.
-		Planned and conducted the preparatory weekends and moderated plenary sessions.
+		◊; Led a team (of group leaders and children) over the course of the 3-week kid's camp.
+		◊; Planned and conducted the preparatory weekends and moderated plenary sessions.
 		◊; Feste?
 	}
 	◊par[#:org "" #:area "" #:role "Group Leader – eight times" #:date "" #:loc "Tübingen" #:topics ""]{
 		
-		Paedagogically supervised 12-20 children.
+		◊; Paedagogically supervised 12-20 children.
 	}
 	◊par[#:org "active group GmbH" #:area "Software Development" #:role "Intern" #:date "03/2020-05/2020" #:loc "Tübingen" #:topics "Haskell, Clojure, Machine Learning, Emacs, magit"]{
 
@@ -71,18 +75,18 @@
 		Developed KI-Agents for the Game Blobby VR.
 		Looked after ◊;{managed?} the TNG booth at OOP 2020.
 	}
-	◊par[#:org "bild der wissenschaft/natur" #:area "Editorial Office" #:role "Intern" #:date "5 days | 09/2018" #:loc "Leinfelden-Echterdingen" #:topics ""]{
+	◊; ◊par[#:org "bild der wissenschaft/natur" #:area "Editorial Office" #:role "Intern" #:date "5 days | 09/2018" #:loc "Leinfelden-Echterdingen" #:topics ""]{
 
 
-	}
-	◊par[#:org "Boehringer Ingelheim" #:area "Pharmacology" #:role "Intern" #:date "5 days | 09/2017" #:loc "Biberach a.d. Riß" #:topics ""]{
+	◊; }
+	◊; ◊par[#:org "Boehringer Ingelheim" #:area "Pharmacology" #:role "Intern" #:date "5 days | 09/2017" #:loc "Biberach a.d. Riß" #:topics ""]{
 
 
-	}
-	◊par[#:org "SmiTo GmbH" #:area "Engineering" #:role "Intern" #:date "5 days | 10/2015" #:loc "Tübingen" #:topics ""]{
+	◊; }
+	◊; ◊par[#:org "SmiTo GmbH" #:area "Engineering" #:role "Intern" #:date "5 days | 10/2015" #:loc "Tübingen" #:topics ""]{
 
 
-	}
+	◊; }
 	◊; ◊par[#:org "" #:area "" #:role "" #:date "" #:loc "" #:topics ""]{
 
 
@@ -126,10 +130,10 @@
 			}
 		}
 	}
-	◊par[#:org "Grundschule Hechinger Eck" #:area "" #:role "" #:date "2006-2010" #:loc "Tübingen" #:topics ""]{
+	◊; ◊par[#:org "Grundschule Hechinger Eck" #:area "" #:role "" #:date "2006-2010" #:loc "Tübingen" #:topics ""]{
 
 
-	}
+	◊; }
 	◊; ◊par[#:org "" #:area "" #:role "" #:date "" #:loc "" #:topics ""]{
 
 
