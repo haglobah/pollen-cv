93251e200dec0baaab5df66845be8f0ad4965299
Beat Hagenlocher <bah@posteo.de>
2026-05-16 14:17:22 +0200

    nfu, then pin packages

---
+++ info.rkt
@@ -0,0 +1,6 @@
+#lang info
+(define collection "pollen-cv")
+(define deps
+  '("base"
+    "https://gitlab.com/mbutterick/pollen.git#ab7cce0b12a8f2a36f84c785c545f5dd9d5d54d7"
+    "https://gitlab.com/mbutterick/txexpr.git#4ed851459e97be03418b1e92a5c07174e99bf28f"))
