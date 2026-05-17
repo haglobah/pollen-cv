b59008f3a38dcfc777596eb307236c8ce4e2bce3
Beat Hagenlocher <bah@posteo.de>
2026-05-17 15:18:41 +0200

    Put in all feedback

--- Résumé.poly.pm
+++ Résumé.poly.pm
@@ -3,9 +3,7 @@
 ◊title[#:street "Dorfstraße 62," #:city "72074 Tübingen, Germany" #:phone "+49 1573 6830713" #:email "bah@posteo.de" #:website "https://me.beathagenlocher.com" #:github "https://github.com/haglobah" #:linkedin "https://linkedin.com/in/beat-hagenlocher/"]{Beat Hagenlocher}
 
 ◊statement{
-  An EA and Rationalist concerned about the world's most pressing problems.
-  ◊br{} ◊br{}
-  Pivoting my main job from building career capital to having impact.
+  An EA and Rationalist looking to apply career capital in Software Engineering, Teaching, (Web) Design and Ops to have an impact on the world's most pressing problems. Like: Founding an AI Safety Fellowship, or pulling more people into better careers by serving them better on 80000hours.org.
 }
 
 ◊section["Skills & Abilities"]{
@@ -16,13 +14,14 @@
   }
 
   ◊par[#:roles '("Design Thinking/User Experience")]{
-    Have conducted various Customer Development Interviews (for rootlinks, for zelium, for humane.tools, at Hackathons)
+    Customer Development Interviews and Design Thinking (at rootlinks, zelium, humane.tools) ◊br{}
+    Design Samples: ◊link["https://beathagenlocher.com"]{beathagenlocher.com}, ◊link["https://todos.humane.tools"]{todos.humane.tools}, ◊link["https://haglobah.github.io/Mastering-42"]{Mastering 42}
 
-    ◊themes{Customer Development, Design Thinking, Systems Practice, The Mom Test, The Heart of Innovation}
+    ◊themes{Customer Development, Web Design, Design Thinking, Systems Practice, The Mom Test, The Heart of Innovation}
   }
 
   ◊par[#:roles '("Growth")]{
-    Analytics (event tracking, funnels, user paths) for my ◊link["https://beathagenlocher.com"]{digital garden} and at ◊link["https://active-group.de"]{Active Group GmbH}
+    Analytics (event tracking, funnels, user paths) for getting to know my readers' needs on my ◊link["https://beathagenlocher.com"]{digital garden} and for driving a website redesign at ◊link["https://active-group.de"]{Active Group GmbH}
 
     ◊themes{PostHog, Plausible}
   }
@@ -36,38 +35,31 @@
 
 ◊section["Work Experience"]{
 
-  ◊par[#:org "AI Safety Tübingen" #:url "https://aisafetytuebingen.com" #:area "AI Safety Fellowship" #:roles '("Project Lead") #:date "02/2026-Present" #:loc "Tübingen" #:topics "Operations" #:show-tops? #t]{
-    Doing what's needed to make it happen: Gathering support from the ELLIS Institute/the Max Planck Institute, writing the grant application for CG.
+  ◊par[#:org "AI Safety Tübingen" #:url "https://aisafetytuebingen.com" #:area "AI Safety Fellowship" #:roles '("Project Lead: AI Safety Fellowship") #:date "02/2026-Present" #:loc "Tübingen" #:topics "Operations" #:show-tops? #t]{
+    Navigating the founding of the fellowship (3 months, 20 fellows with stipends & housing) and making decisions under uncertainty: Finding mentors, organizing housing, estimating a budget, gathering support from stakeholders like the ELLIS Institute/the Max Planck Institute, writing the grant application for CG.
   }
 
   ◊par[#:org "humane.tools" #:url "https://humane.tools" #:area "Startup" #:roles '("Founder") #:date "01/2026-Present" #:loc "Tübingen" #:topics "Software Architecture, TypeScript, Nix, SolidJS, TailwindCSS, Local-first Software, Design"]{
-  Building the tools I've always wanted: A ◊link["https://todos.humane.tools"]{better todo app}, a better Spaced Repetition System. Built the todo app from scratch, conducted Customer Development Interviews.
+  Building better tools: A ◊link["https://todos.humane.tools"]{todo app}, a Spaced Repetition System, and maybe more. Built the todo app from scratch, and iterated through learnings from Customer Development Interviews.
 
   }
-  ◊par[#:org "Active Group GmbH" #:url "https://www.active-group.de" #:area "Software Architecture" #:roles '("Software Architect") #:date "05/2024-Present" #:loc "Tübingen/Remote" #:topics "Software Architecture, TypeScript, Clojure, ClojureScript, Docker, Nix, SolidJS, UnoCSS, TailwindCSS, Emacs, Neovim"]{
-    Writing Software for German industrial clients: Worked on a monitoring platform for a factory in Clojure, and a user-facing map application with a SolidJS/TypeScript on the frontend, and Clojure on the backend.
+  ◊par[#:org "Active Group GmbH" #:url "https://www.active-group.de" #:area "Software Architecture" #:roles '("Software Architect") #:date "05/2024-Present" #:loc "Tübingen/Remote" #:topics "Software Architecture, TypeScript, Clojure, ClojureScript, Docker, Nix, SolidJS, TailwindCSS"]{
+    Writing software for German industrial clients: A monitoring platform for a factory in Clojure, and a user-facing map application with a SolidJS/TypeScript frontend, and a Clojure backend.
   }
   ◊par[#:org "zelium.io" #:url "https://zelium.io" #:area "Startup" #:roles '("Startup Founder") #:date "09/2023-05/2024" #:loc "Tübingen" #:topics "Systems Practice, Design Thinking, Elixir, Phoenix LiveView, Tailwind CSS, CodeMirror, Nix"]{
 
     ◊ul{
-      ◊li{Applied ◊l["https://oecd-opsi.org/toolkits/systems-practice-workbook/"]{Systems Practice} to the problem of digital collaboration}
-      ◊li{Identified a gap: A collaboration tool that starts very simple, makes sharing easy, but lets you scale up to scheduling, meeting protocols and more }
-      ◊li{Conducted Customer Development Interviews and built low-fidelity propotypes}
-      ◊li{Built an MVP}
+      ◊li{Applied ◊l["https://oecd-opsi.org/toolkits/systems-practice-workbook/"]{Systems Practice} to the problem of digital collaboration. Identified a gap: A tool that starts simple, makes project sharing easy, but lets you scale up to scheduling, meeting protocols and more}
+      ◊li{Conducted ~10 Customer Development Interviews and built low-fidelity prototypes, prioritized features, built an MVP out of them}
     }
   }
   ◊par[#:org "Hacker School gGmbH" #:url "https://hacker-school.de" #:area "NGO: Education" #:roles '("Working Student") #:date "02/2021-03/2023" #:loc "Hamburg/Remote" #:topics "Sociocracy, Racket, Teaching"]{
-    Created and a course website with ◊l["https://pollenpub.com"]{Pollen}, created courses, both used by thousands of students.
+    Created a course website and courses with ◊l["https://pollenpub.com"]{Pollen}. Both used by thousands of students.
 
   }
   ◊par[#:org "rootlinks" #:url "https://www.rootlinks.ch" #:area "NGO: Education, Sustainability" #:roles '("Organizer") #:date "06/2021-03/2022" #:loc "Zürich" #:topics "Sociocracy, Sustainability, Design Thinking, Systems Design, Teaching, Coaching, Recruiting"]{
 
-    ◊ul{
-      ◊li{Organized the Learning Journey of two semesters, held inputs and workshops}
-      ◊li{Mentored a challenge team}
-      ◊li{Contributed to creating—and evolving—a working organizational structure}
-      ◊li{Planned and executed the Recruiting of the 3rd batch.}
-    }
+    Organized the Learning Journey of two semesters, held inputs and workshops, mentored a challenge team, redesigned the organizational structure, planned and executed the recruiting of the 3rd batch.
   }
   ◊; ◊par[#:org "Active Group GmbH" #:url "https://www.active-group.de" #:area "Software Development" #:roles '("Intern") #:date "03/2020-05/2020" #:loc "Tübingen" #:topics "Haskell, Clojure, Machine Learning, Emacs, Magit, Pair Programming"]{
 
@@ -87,12 +79,12 @@
 
   ◊; }
 
-  ◊; ◊par[#:org "Deutsche SchülerAkademie" #:url "https://www.deutsche-schuelerakademien.de" #:area "" #:roles '("Course Instructor") #:date "17 days | 07/2025" #:loc "Roßleben" #:topics "Science, Teaching" #:show-tops? #t]{
+  ◊par[#:org "Deutsche SchülerAkademie" #:url "https://www.deutsche-schuelerakademien.de" #:area "" #:roles '("Course Instructor") #:date "17 days each | 07/2025 + 07/2022" #:loc "Roßleben" #:topics "Science, Teaching, Learning" #:show-tops? #t]{
 
-  ◊;   Designed and taught a 50-hour university-level course about empirical methods and science.
+    Designed and taught two 50-hour university-level courses for 16 high-achieving students ages 16-19: One about empirical methods and science, and one about the science of learning.
 
   ◊;   ◊; Taught a 50-hour university level course called ◊e{How People Learn} to exceptionally strong students (age 16-18). Co-supervised all the students over the course of the academy.
-  ◊; }
+  }
 
   ◊; ◊par[#:org "Deutsche SchülerAkademie" #:url "https://www.deutsche-schuelerakademien.de" #:area "" #:roles '("Course Instructor") #:date "17 days | 06-07/2022" #:loc "Wolfsberg" #:topics "Learning, Teaching" #:show-tops? #t]{
 
@@ -139,11 +131,12 @@
     ◊(br)◊(br)
     ◊e{03/21 - 07/21 Participation in the ◊l["https://www.rootlinks.ch"]{rootlinks}} programme
   }
-  ◊par[#:org "Leibniz Kolleg" #:area "" #:roles '("Studium generale et sociale") #:date "10/2018-07/2019" #:loc "Tübingen" #:topics ""]{
+  ◊par[#:org "Leibniz Kolleg" #:url "https://uni-tuebingen.de/einrichtungen/verwaltung/stabsstellen/leibniz-kolleg/das-leibniz-kolleg/" #:area "" #:roles '("Studium generale et sociale") #:date "10/2018-07/2019" #:loc "Tübingen" #:topics ""]{
 
+    Highly selective orientational study program with self-organization while living together as 50 people.
     ◊;{Obligatory seminars: Scientific Writing, Rhetorics, Writing for science, Seminar for scientific posters, Politics (in collaboration with lpb Baden-Württemberg), study trip to rome with focus on arts and archeology
     ◊(br)◊(br)}
-    Courses: Psychology, Medicine, Virology, Philosophy, Choir, Astrophysics, Sinology, Education, Theory of Science and Philosophy of Mind, Economics, Mathematics, Computer Science, Theater
+    ◊; Courses: Psychology, Medicine, Virology, Philosophy, Choir, Astrophysics, Sinology, Education, Theory of Science and Philosophy of Mind, Economics, Mathematics, Computer Science, Theater
     ◊;{◊(br)◊(br)
     Two written term papers:
     ◊ul{
