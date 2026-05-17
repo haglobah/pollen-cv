5a3e9a3ba20e8853ea4b12b9ac8c3ef6c2fd71ea
Beat Hagenlocher <bah@posteo.de>
2026-05-17 15:42:46 +0200

    Adjust wording issues

--- Résumé.poly.pm
+++ Résumé.poly.pm
@@ -3,14 +3,14 @@
 ◊title[#:street "Dorfstraße 62," #:city "72074 Tübingen, Germany" #:phone "+49 1573 6830713" #:email "bah@posteo.de" #:website "https://me.beathagenlocher.com" #:github "https://github.com/haglobah" #:linkedin "https://linkedin.com/in/beat-hagenlocher/"]{Beat Hagenlocher}
 
 ◊statement{
-  An EA and Rationalist looking to apply career capital in Software Engineering, Teaching, (Web) Design and Ops to have an impact on the world's most pressing problems. Like: Founding an AI Safety Fellowship, or pulling more people into better careers by serving them better on 80000hours.org.
+  An EA and Rationalist looking to apply career capital in Software Engineering, Teaching, (Web) Design and Ops to have an impact on the world's most pressing problems. Like: founding an AI Safety Fellowship, or pulling more people into better careers by serving them better on 80000hours.org.
 }
 
 ◊section["Skills & Abilities"]{
 
   ◊par[#:roles '("Software Engineering")]{
 
-    ◊themes{Frontend: TypeScript | SolidJS | TailwindCSS, Backend: Clojure | Elixir | Haskell | SQL, Nix, Software Architecture, Functional Programming, Local-first Software}
+    ◊themes{Frontend: TypeScript | SolidJS | TailwindCSS, Backend: Clojure | Elixir | Haskell | SQL, Nix, Docker, Software Architecture, Functional Programming, Local-first Software}
   }
 
   ◊par[#:roles '("Design Thinking/User Experience")]{
@@ -21,13 +21,13 @@
   }
 
   ◊par[#:roles '("Growth")]{
-    Analytics (event tracking, funnels, user paths) for getting to know my readers' needs on my ◊link["https://beathagenlocher.com"]{digital garden} and for driving a website redesign at ◊link["https://active-group.de"]{Active Group GmbH}
+    Analytics (event tracking, funnels, user paths) to understand my readers' needs on my ◊link["https://beathagenlocher.com"]{digital garden} and to drive a website redesign at ◊link["https://active-group.de"]{Active Group GmbH}
 
     ◊themes{PostHog, Plausible}
   }
 
   ◊par[#:roles '("Writing & Teaching")]{
-    Both in my ◊link["https://beathagenlocher.com"]{digital garden}, for ◊link["https://funktionale-programmierung.de/en/2026/03/04/fp-by-example-refactoring-typescript-explicit-model.html"]{work}, for ◊link["https://www.schuelerakademien.de/programm/kurs?tx_ewacademy_coursedetail%5Bcontroller%5D=Course&tx_ewacademy_coursedetail%5Bcourse%5D=4817&cHash=04644e2bcdc91409f905091d0161cd9e"]{courses} and inside my personal knowledge base.
+    In my ◊link["https://beathagenlocher.com"]{digital garden}, for ◊link["https://funktionale-programmierung.de/en/2026/03/04/fp-by-example-refactoring-typescript-explicit-model.html"]{work}, for ◊link["https://www.schuelerakademien.de/programm/kurs?tx_ewacademy_coursedetail%5Bcontroller%5D=Course&tx_ewacademy_coursedetail%5Bcourse%5D=4817&cHash=04644e2bcdc91409f905091d0161cd9e"]{courses} and inside my personal knowledge base.
     ◊themes{Style: Towards Clarity and Grace, Practical Typography}
   }
 
@@ -36,21 +36,21 @@
 ◊section["Work Experience"]{
 
   ◊par[#:org "AI Safety Tübingen" #:url "https://aisafetytuebingen.com" #:area "AI Safety Fellowship" #:roles '("Project Lead: AI Safety Fellowship") #:date "02/2026-Present" #:loc "Tübingen" #:topics "Operations" #:show-tops? #t]{
-    Navigating the founding of the fellowship (3 months, 20 fellows with stipends & housing) and making decisions under uncertainty: Finding mentors, organizing housing, estimating a budget, gathering support from stakeholders like the ELLIS Institute/the Max Planck Institute, writing the grant application for CG.
+    Navigating the founding of the fellowship (3 months, 20 fellows with stipends & housing) and making decisions under uncertainty: Finding mentors, organizing housing, estimating a budget, gathering support from stakeholders like the ELLIS Institute/the Max Planck Institute, writing the grant application for Coefficient Giving.
   }
 
   ◊par[#:org "humane.tools" #:url "https://humane.tools" #:area "Startup" #:roles '("Founder") #:date "01/2026-Present" #:loc "Tübingen" #:topics "Software Architecture, TypeScript, Nix, SolidJS, TailwindCSS, Local-first Software, Design"]{
-  Building better tools: A ◊link["https://todos.humane.tools"]{todo app}, a Spaced Repetition System, and maybe more. Built the todo app from scratch, and iterated through learnings from Customer Development Interviews.
+  Building better tools: A ◊link["https://todos.humane.tools"]{todo app}, a Spaced Repetition System. Built the todo app from scratch, and iterated through learnings from Customer Development Interviews.
 
   }
   ◊par[#:org "Active Group GmbH" #:url "https://www.active-group.de" #:area "Software Architecture" #:roles '("Software Architect") #:date "05/2024-Present" #:loc "Tübingen/Remote" #:topics "Software Architecture, TypeScript, Clojure, ClojureScript, Docker, Nix, SolidJS, TailwindCSS"]{
-    Writing software for German industrial clients: A monitoring platform for a factory in Clojure, and a user-facing map application with a SolidJS/TypeScript frontend, and a Clojure backend.
+    Writing software for German industrial clients: A Clojure monitoring platform for a factory, and a user-facing map application with a SolidJS/TypeScript frontend, and a Clojure backend.
   }
   ◊par[#:org "zelium.io" #:url "https://zelium.io" #:area "Startup" #:roles '("Startup Founder") #:date "09/2023-05/2024" #:loc "Tübingen" #:topics "Systems Practice, Design Thinking, Elixir, Phoenix LiveView, Tailwind CSS, CodeMirror, Nix"]{
 
     ◊ul{
-      ◊li{Applied ◊l["https://oecd-opsi.org/toolkits/systems-practice-workbook/"]{Systems Practice} to the problem of digital collaboration. Identified a gap: A tool that starts simple, makes project sharing easy, but lets you scale up to scheduling, meeting protocols and more}
-      ◊li{Conducted ~10 Customer Development Interviews and built low-fidelity prototypes, prioritized features, built an MVP out of them}
+      ◊li{Applied ◊l["https://oecd-opsi.org/toolkits/systems-practice-workbook/"]{Systems Practice} to the problem of digital collaboration. Identified a gap: A tool that starts simple, makes project sharing easy, but lets you scale up to scheduling, meeting protocols and more.}
+      ◊li{Conducted ~10 Customer Development Interviews and built low-fidelity prototypes, prioritized features, shipped an MVP.}
     }
   }
   ◊par[#:org "Hacker School gGmbH" #:url "https://hacker-school.de" #:area "NGO: Education" #:roles '("Working Student") #:date "02/2021-03/2023" #:loc "Hamburg/Remote" #:topics "Sociocracy, Racket, Teaching"]{
@@ -59,7 +59,7 @@
   }
   ◊par[#:org "rootlinks" #:url "https://www.rootlinks.ch" #:area "NGO: Education, Sustainability" #:roles '("Organizer") #:date "06/2021-03/2022" #:loc "Zürich" #:topics "Sociocracy, Sustainability, Design Thinking, Systems Design, Teaching, Coaching, Recruiting"]{
 
-    Organized the Learning Journey of two semesters, held inputs and workshops, mentored a challenge team, redesigned the organizational structure, planned and executed the recruiting of the 3rd batch.
+    Organized the Learning Journey of two semesters, gave talks, ran workshops, mentored a challenge team, redesigned the organizational structure, planned and executed the recruiting of the 3rd batch.
   }
   ◊; ◊par[#:org "Active Group GmbH" #:url "https://www.active-group.de" #:area "Software Development" #:roles '("Intern") #:date "03/2020-05/2020" #:loc "Tübingen" #:topics "Haskell, Clojure, Machine Learning, Emacs, Magit, Pair Programming"]{
 
@@ -133,7 +133,7 @@
   }
   ◊par[#:org "Leibniz Kolleg" #:url "https://uni-tuebingen.de/einrichtungen/verwaltung/stabsstellen/leibniz-kolleg/das-leibniz-kolleg/" #:area "" #:roles '("Studium generale et sociale") #:date "10/2018-07/2019" #:loc "Tübingen" #:topics ""]{
 
-    Highly selective orientational study program with self-organization while living together as 50 people.
+    Highly selective orientational study program combined with group housing self-organization (50 people).
     ◊;{Obligatory seminars: Scientific Writing, Rhetorics, Writing for science, Seminar for scientific posters, Politics (in collaboration with lpb Baden-Württemberg), study trip to rome with focus on arts and archeology
     ◊(br)◊(br)}
     ◊; Courses: Psychology, Medicine, Virology, Philosophy, Choir, Astrophysics, Sinology, Education, Theory of Science and Philosophy of Mind, Economics, Mathematics, Computer Science, Theater
